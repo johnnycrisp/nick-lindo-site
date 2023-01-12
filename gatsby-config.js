af4095d7db00da76806f2ba24c98c50589e7b7e0
Johnny Crisp <johnnycrisp2@gmail.com>
2023-01-12 15:28:52 +0100

    initial files

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-datocms',
     options: {
-      "apiToken": "765663368f3ca40a6301f88b73d15e"
+      apiToken: process.env.DATOCMS_API_TOKEN,
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
     resolve: 'gatsby-source-filesystem',
