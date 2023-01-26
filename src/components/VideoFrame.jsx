98f7041f3a4d89d5943ec2b210997b2701d2966a
Johnny Crisp <johnnycrisp2@gmail.com>
2023-01-26 11:22:49 +0100

    video frame loop

--- src/components/VideoFrame.jsx
+++ src/components/VideoFrame.jsx
@@ -1,22 +1,20 @@
-import React from "react"
-const VideoFrame = ({ component }) => { 
+import React from "react";
+const VideoFrame = ({ component }) => {
+  const videoId = component.videoLink.url.slice(-11);
 
-    const videoId = component.videoLink.url.slice(- 11)
-
-   return (
-
-  <div className="video">
-    <iframe
-      src={`https://www.youtube.com/embed/${videoId}`}
-      title={component.videoLink.title}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share"
-      frameBorder="0"
-      webkitallowfullscreen="true"
-      mozallowfullscreen="true"
-      rel="0"
-      allowFullScreen
-    />
-  </div>
-)
-}
-export default VideoFrame
+  return (
+    <div className="video">
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?playlist=${videoId}&loop=1`}
+        title={component.videoLink.title}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share"
+        frameBorder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        rel="0"
+        allowFullScreen
+      />
+    </div>
+  );
+};
+export default VideoFrame;
--- src/templates/campaign.jsx
+++ src/templates/campaign.jsx
@@ -1,89 +1,85 @@
-import { graphql } from 'gatsby'
-import React from 'react'
-import CampaignHeader from '../components/CampaignHeader'
-import ComponentSelector from '../components/ComponentSelector'
-import Layout from '../components/Layout'
-import Pagination from '../components/Pagination'
-import Seo from '../components/Seo'
+import { graphql } from "gatsby";
+import React from "react";
+import CampaignHeader from "../components/CampaignHeader";
+import ComponentSelector from "../components/ComponentSelector";
+import Layout from "../components/Layout";
+import Pagination from "../components/Pagination";
+import Seo from "../components/Seo";
 
+const Campaign = ({ data }) => {
+  const campaign = data.campaign;
 
-const Campaign = ({data}) => {
-    const campaign = data.campaign
- 
+  const blocks = campaign.campaignContent[0]?.campaignBlocks;
 
-    const blocks = campaign.campaignContent[0]?.campaignBlocks
-
-    // console.log('campaign', campaign)
-    // console.log('blocks', blocks)
+  // console.log('campaign', campaign)
+  // console.log('blocks', blocks)
 
   return (
     <Layout>
-    <Seo title={campaign.title} />
-        <CampaignHeader campaign={campaign} />
-        <ComponentSelector 
-          components={blocks}
-        />
-        <Pagination campaign={campaign}/>
+      <Seo title={campaign.title} />
+      <CampaignHeader campaign={campaign} />
+      <ComponentSelector components={blocks} />
+      <Pagination campaign={campaign} />
     </Layout>
-    
-  )
-}
+  );
+};
 
-export default Campaign
+export default Campaign;
 
 export const query = graphql`
-query CampaignTitleQuery($pageId: String) {
-  campaign: datoCmsCampaign(originalId: {eq: $pageId}) {
-    originalId
-    title
-    subtitle
-    campaignDetails
-    position
-    slug
-    introText
-    coverImage {
+  query CampaignTitleQuery($pageId: String) {
+    campaign: datoCmsCampaign(originalId: { eq: $pageId }) {
+      originalId
+      title
+      subtitle
+      campaignDetails
+      position
+      slug
+      introText
+      coverImage {
         gatsbyImageData(placeholder: BLURRED)
-    }
-    campaignContent {
-      campaignBlocks {
-        ... on DatoCmsTextSection {
-          id
-          text
-          subtitle
-          title
-          internal {
-            type
-          }
-        }
-        ... on DatoCmsSingleVideo {
-          id
-          internal {
-            type
-          }
-          videoLink {
-            url
+      }
+      campaignContent {
+        campaignBlocks {
+          ... on DatoCmsTextSection {
+            id
+            text
+            subtitle
             title
+            internal {
+              type
+            }
           }
-        }
-        ... on DatoCmsImageGallery {
-          id
-          internal {
-            type
+          ... on DatoCmsSingleVideo {
+            id
+            internal {
+              type
+            }
+            videoLink {
+              url
+              title
+            }
           }
-          images {
-            gatsbyImageData(placeholder: BLURRED)
+          ... on DatoCmsImageGallery {
+            id
+            internal {
+              type
+            }
+            images {
+              gatsbyImageData(placeholder: BLURRED)
+            }
           }
-        }
-        ... on DatoCmsSingleImage {
-          id
-          internal {
-            type
-          }
-          singleImage {
-            gatsbyImageData(placeholder: BLURRED)
+          ... on DatoCmsSingleImage {
+            id
+            internal {
+              type
+            }
+            singleImage {
+              gatsbyImageData(placeholder: BLURRED)
+            }
           }
         }
       }
     }
   }
-}`
+`;
